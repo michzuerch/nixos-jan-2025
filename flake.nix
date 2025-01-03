bbc1d3b37da3a107be9085ae8c730460a0c81463
Michael Zuercher <michzuerch@gmail.com>
2025-01-03 10:48:16 +0100

    nix flake check ok

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
+      # url = "github:nix-community/home-manager";
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -35,7 +36,7 @@
       }
       {
         hostname = "slim3";
-        stateVersion = "24.05";
+        stateVersion = "24.11";
       }
       {
         hostname = "330-15ARR";
