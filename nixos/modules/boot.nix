1c89fa76e3d2a40a1ceef54f8e2861da65f22c70
Michael Zuercher <michzuerch@gmail.com>
2025-01-14 17:16:23 +0100

    change boot loader

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,4 +1,8 @@
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
 }
