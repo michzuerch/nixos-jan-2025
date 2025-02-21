d7bee2bfe65f5f53564d47928ce06558d22f2183
Michael Zuercher <michzuerch@gmail.com>
2025-02-21 12:09:47 +0100

    new disko config from sample sasha koenig

--- hosts/vm/disko.nix
+++ hosts/vm/disko.nix
@@ -1,20 +1,23 @@
 {
   disko.devices = {
     disk = {
-      main = {
-        device = "/dev/vda";
+      nixos = {
         type = "disk";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
             ESP = {
+              size = "512M";
               type = "EF00";
-              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
               };
             };
             root = {
