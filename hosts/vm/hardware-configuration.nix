bcce0a68502592c427a4c2f417f837b28233483f
Michael Zuercher <michzuerch@gmail.com>
2025-02-21 16:42:54 +0100

    typo virtio_blk

--- hosts/vm/hardware-configuration.nix
+++ hosts/vm/hardware-configuration.nix
@@ -12,7 +12,7 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtuo_blk"];
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
