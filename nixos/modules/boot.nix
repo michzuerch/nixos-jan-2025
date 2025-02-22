d9939a2d18493b3a378e0ed1fed301e2297c343a
Michael Zuercher <michzuerch@gmail.com>
2025-02-22 12:28:52 +0100

    bootloader to grub

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,8 +1,8 @@
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
 
-  #boot.loader.grub.enable = true;
-  #boot.loader.grub.efiSupport = true;
-  #boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
 }
